dcc4b1bc10ea8988ff0f0bac7fab5fcba0efa313
Meelis Mumm <mumm.meelis@gmail.com>
2024-06-08 21:47:04 +0300

    added dupe user check and pw hashing

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.22.3
 require (
 	github.com/google/uuid v1.6.0
 	github.com/mattn/go-sqlite3 v1.14.22
+	golang.org/x/crypto v0.24.0
 )
--- internal/services/user.go
+++ internal/services/user.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserService interface {
@@ -20,9 +21,33 @@ func NewUserService(db *sql.DB) UserService {
 }
 
 func (uds *UserDatabaseService) Create(username, email, password string, userId uuid.UUID) error {
-	_, err := uds.Database.Exec("INSERT INTO users (id, username, email, password) VALUES (?, ?, ?, ?)", userId, username, email, password)
+
+	userExists, err := uds.isRegistered(email, userId)
+	if err != nil {
+		return fmt.Errorf("failed to check if user exists: %v", err)
+	}
+
+	if userExists {
+		return fmt.Errorf("user already exists")
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return fmt.Errorf("failed to hash the password: %v", err)
+	}
+
+	_, err = uds.Database.Exec("INSERT INTO users (id, username, email, password) VALUES (?, ?, ?, ?)", userId, username, email, string(hashedPassword))
 	if err != nil {
 		return fmt.Errorf("failed to insert user: %v", err)
 	}
 	return nil
+}
+
+func (uds *UserDatabaseService) isRegistered(email string, userId uuid.UUID) (bool, error) {
+	var count int
+	err := uds.Database.QueryRow(`SELECT COUNT(*) FROM users WHERE email = ? or id = ?`, email, userId).Scan(&count)
+	if err != nil {
+		return false, fmt.Errorf("failed to check if email or id exists: %v", err)
+	}
+	return count > 0, nil
 }
