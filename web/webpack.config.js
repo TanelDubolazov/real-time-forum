0fe531c38d8b0a292a400390382f7743b9b6a161
Meelis Mumm <mumm.meelis@gmail.com>
2024-06-19 19:52:13 +0300

    added webpack serve to npm package

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,41 +1,42 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  // Entry point of your application
   entry: "./app.js",
-
-  // Output configuration for Webpack
   output: {
-    path: path.resolve(__dirname, "dist"), // The output directory
-    filename: "app.js", // The name of the output file
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
-
-  // Set the mode to 'development' or 'production'
-  mode: "production",
-
-  // Configuration for modules
+  mode: "development",
   module: {
     rules: [
       {
-        test: /\.js$/, // Regex for JavaScript files
-        exclude: /node_modules/, // Exclude the node_modules directory
+        test: /\.js$/,
+        exclude: /node_modules/,
         use: {
-          loader: "babel-loader", // Use babel-loader for transpiling JavaScript
+          loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-env"], // Use the env preset
+            presets: ["@babel/preset-env"],
           },
         },
       },
       {
-        test: /\.css$/, // Regex for CSS files
-        use: ["style-loader", "css-loader"], // Use these loaders for CSS files
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
       },
-      // Add more rules for other file types (e.g., images, fonts)
     ],
   },
-
-  // Plugins (if any)
   plugins: [
-    // Add any plugins you need here
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
   ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname),
+    },
+    compress: true,
+    port: 8081,
+    historyApiFallback: true,
+  },
 };
